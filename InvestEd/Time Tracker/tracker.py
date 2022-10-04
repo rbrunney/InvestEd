7e81c99d04aeeb5bfa9591c4c7abda9603956624
rbrunney <rbrunney@student.neumont.edu>
2022-10-03 21:50:23 -0600

    Add Hours Activity Visual Done
    Just need to add hours to json file to finish up add hours

--- InvestEd/Time Tracker/tracker.py
+++ InvestEd/Time Tracker/tracker.py
@@ -1,21 +1,88 @@
 import PySimpleGUI as gui
+import os
 
 def main():
-    gui.theme("Dark Amber")
 
-    window_components = [
-        [gui.Text('Add Time')],
+    gui.theme("Python")
+
+    ## Making First Column Layout for Adding Hours 
+
+    # Fields for giving activity title
+    add_column_1 = [
+        [gui.Text('Activity', justification='right')],
+        [gui.Combo(values=['Coding', 'Research', 'Design', 'Bug Fixing', 'Other'],  key='-ACTIVITYBOX-', readonly=True)]
     ]
 
-    window = gui.Window(title='Capstone Time Tracker', layout=window_components, margins=(400, 200))
+    # Fields for Start and End Time
+
+    add_column_2 = [
+        [gui.Text('Start Time', justification='center')],
+        [
+            gui.Combo(values=[hour for hour in range(1, 13)], key='-S-HOUR-', default_value=12, readonly=True), 
+            gui.Text(':'), 
+            gui.Combo(values=[hour for hour in range(1, 60)], key='-S-MINUTE-', default_value=30, readonly=True),
+            gui.Combo(values=['am', 'pm'], key='-S-DAYTIME-', default_value='am', readonly=True)
+        ],
+        [gui.Text('End Time', justification='center')],
+        [
+            gui.Combo(values=[hour for hour in range(1, 13)], key='-E-HOUR-', default_value=12, readonly=True), 
+            gui.Text(':'), 
+            gui.Combo(values=[hour for hour in range(1, 60)], key='-E-MINUTE-', default_value=30, readonly=True),
+            gui.Combo(values=['am', 'pm'], key='-E-DAYTIME-', default_value='am', readonly=True)
+        ],
+    ]
+
+    week_list = [f'Week {week}' for week in range(1, 11)]
+    add_hours_layout = [
+        [gui.Column(layout=add_column_1), gui.Column(add_column_2)],
+        [gui.Text('Description')],
+        [gui.Multiline(key='-MULTILINE-')],
+        [gui.Combo(week_list, default_value=week_list[0], readonly=True, key='-WEEK-')],
+        [gui.Button('Add Hours', key='-ADD-')]
+    ]
+
+    view_hours_layout = [[gui.Text('Hello from view')]]
+
+    window_components = [
+        [
+            gui.TabGroup(
+                [[
+                    gui.Tab('Add Hours', layout=add_hours_layout),
+                    gui.Tab('View Hours', layout=view_hours_layout)
+                ]],
+                tab_location='top'
+            )
+        ]
+    ]
+
+    window = gui.Window(title='Capstone Time Tracker', layout=window_components, margins=(300, 150))
 
     while True:
         event, values = window.read()
 
+        if event == '-ADD-':
+            # Creating activity for information
+            create_activity(
+                window['-WEEK-'].get(), window['-ACTIVITYBOX-'].get(),
+                str(window['-S-HOUR-'].get()) + ":" + str(window['-S-MINUTE-'].get()) + ' ' + window['-S-DAYTIME-'].get(),
+                str(window['-E-HOUR-'].get()) + ":" + str(window['-E-MINUTE-'].get()) + ' ' + window['-E-DAYTIME-'].get(),
+                window['-MULTILINE-'].get()
+            )
         if event == gui.WIN_CLOSED:
             break
 
     window.close()
+
+def create_activity(week, activity, start_time, end_time, description):
+    new_activity = {
+        "activity" : activity,
+        "start_time" : start_time,
+        "end_time" : end_time,
+        "description" : description
+    }
+
+    print(new_activity)
+
 
 if __name__ == "__main__":
     main()
