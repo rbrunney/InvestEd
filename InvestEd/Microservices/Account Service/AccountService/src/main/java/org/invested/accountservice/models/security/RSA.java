c8e40ea4585ba299450b264278a93e97b02fd62d
rbrunney <rbrunney@student.neumont.edu>
2022-10-11 10:51:18 -0600

    Updated RSA
    Working on updating RSA to make encryption work and happen

--- InvestEd/Microservices/Account Service/AccountService/src/main/java/org/invested/accountservice/models/security/RSA.java
+++ InvestEd/Microservices/Account Service/AccountService/src/main/java/org/invested/accountservice/models/security/RSA.java
@@ -1,18 +1,21 @@
 package org.invested.accountservice.models.security;
 
 import javax.crypto.Cipher;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.security.KeyFactory;
-import java.security.PrivateKey;
-import java.security.PublicKey;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.*;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.util.Base64;
 
 // Rivest-Shamir-Adleman Algorithm
 public class RSA {
 
-    private final PrivateKey rsaPrivateKey = extractPrivateKey(System.getenv("RSA_PRIVATE_KEY"));
-    private final PublicKey rsaPublicKey = extractPublicKey(System.getenv("RSA_PUBLIC_KEY"));
+    private final PrivateKey rsaPrivateKey = extractPrivateKey();
+    private final PublicKey rsaPublicKey = extractPublicKey();
 
     public String encrypt(String msgToEncrypt) {
         try {
@@ -38,42 +41,55 @@ public class RSA {
         return null;
     }
 
-    private PrivateKey extractPrivateKey(String privateKeyPem) {
-        privateKeyPem = privateKeyPem.replace("-----BEGIN RSA PRIVATE KEY-----", "");
-        privateKeyPem = privateKeyPem.replace("-----END RSA PRIVATE KEY-----", "");
-        privateKeyPem = privateKeyPem.replace(" ", "");
-
-        // Decode the Base64
-        byte[] pkcs8EncodeBytes = Base64.getDecoder().decode(privateKeyPem);
-
-        // Extracting the private key
+    private PublicKey extractPublicKey() {
 
         try {
-            PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(pkcs8EncodeBytes);
-            KeyFactory kf = KeyFactory.getInstance("RSA");
-            return kf.generatePrivate(keySpec);
-        } catch(Exception e) {
-            System.out.println("[ERROR] " + e.getMessage());
+            String privateKeyPem = Files.readString(Path.of("../src/main/resources/certs/privateKey.pem"), Charset.defaultCharset());
+            privateKeyPem = privateKeyPem.replace("-----BEGIN RSA Public KEY-----", "");
+            privateKeyPem = privateKeyPem.replace("-----END RSA Public KEY-----", "");
+            privateKeyPem = privateKeyPem.replace(" ", "");
+
+            // Decode the Base64
+            byte[] pkcs8EncodeBytes = Base64.getDecoder().decode(privateKeyPem);
+
+            // Extracting the private key
+
+            try {
+                PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(pkcs8EncodeBytes);
+                KeyFactory kf = KeyFactory.getInstance("RSA");
+                return kf.generatePublic(keySpec);
+            } catch(Exception e) {
+                System.out.println("[ERROR] " + e.getMessage());
+            }
+        } catch(IOException ioe) {
+            System.out.println("[ERROR] Cannot read file");
         }
 
         return null;
     }
 
-    private PublicKey extractPublicKey(String publicKeyPem) {
-        publicKeyPem = publicKeyPem.replace("-----BEGIN PUBLIC KEY-----", "");
-        publicKeyPem = publicKeyPem.replace("-----END PUBLIC KEY-----", "");
-        publicKeyPem = publicKeyPem.replace("\\s+", "");
+    private PrivateKey extractPrivateKey() {
 
-        // Decode the Base64
-        byte[] pkcs8EncodeBytes = Base64.getDecoder().decode(publicKeyPem);
-
-        // Extracting the private key
         try {
-            PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(pkcs8EncodeBytes);
-            KeyFactory kf = KeyFactory.getInstance("RSA");
-            return kf.generatePublic(keySpec);
-        } catch(Exception e) {
-            System.out.println("[ERROR] " + e.getMessage());
+            String privateKeyPem = Files.readString(Path.of("../src/main/resources/certs/privateKey.pem"), Charset.defaultCharset());
+            privateKeyPem = privateKeyPem.replace("-----BEGIN RSA PRIVATE KEY-----", "");
+            privateKeyPem = privateKeyPem.replace("-----END RSA PRIVATE KEY-----", "");
+            privateKeyPem = privateKeyPem.replace(" ", "");
+
+            // Decode the Base64
+            byte[] pkcs8EncodeBytes = Base64.getDecoder().decode(privateKeyPem);
+
+            // Extracting the private key
+
+            try {
+                PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(pkcs8EncodeBytes);
+                KeyFactory kf = KeyFactory.getInstance("RSA");
+                return kf.generatePrivate(keySpec);
+            } catch(Exception e) {
+                System.out.println("[ERROR] " + e.getMessage());
+            }
+        } catch(IOException ioe) {
+            System.out.println("[ERROR] Cannot read file");
         }
 
         return null;
