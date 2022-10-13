344fa90e74584fd44ef09e69f72114822dc18e1c
rbrunney <rbrunney@student.neumont.edu>
2022-10-12 19:00:44 -0600

    Confirmation Email Consumer
    Email Consumer started, just need to implement the new SendEmail and it will be complete!

---
+++ InvestEd/Microservices/Account Service/ConfirmationEmailConsumer/src/main/java/org/invested/models/rabbitmq/RabbitMQConsumer.java
@@ -0,0 +1,57 @@
+package org.invested.models.rabbitmq;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class RabbitMQConsumer {
+
+    private final Connection currentConnection;
+
+    public RabbitMQConsumer() {
+        currentConnection = makeConnection();
+    }
+
+    private Connection makeConnection() {
+        try {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost(System.getenv("RABBITMQ_HOST"));
+            factory.setPort(Integer.parseInt(System.getenv("RABBITMQ_PORT")));
+            factory.setUsername(System.getenv("RABBITMQ_USERNAME"));
+            factory.setPassword(System.getenv("RABBITMQ_PASSWORD"));
+            return factory.newConnection();
+        } catch(Exception e) {
+            System.out.println("[ERROR] " + e.getMessage());
+            return null;
+        }
+    }
+
+    public void startConsumingQueueMessages() {
+        try {
+            Channel channel = currentConnection.createChannel();
+
+            DefaultConsumer consumer = new DefaultConsumer(channel) {
+                @Override
+                public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) {
+                    String rabbitmqMessage = new String(body, StandardCharsets.UTF_8);
+
+                    System.out.println(rabbitmqMessage);
+                }
+            };
+
+            channel.basicConsume("confirmation-email-queue", true, consumer);
+        } catch(IOException ioe) {
+            System.out.println("[ERROR] " + ioe.getMessage());
+        }
+
+    }
+
+    private void closeConnection() {
+        try {
+            currentConnection.close();
+        } catch(IOException ioe) {
+            System.out.println("[ERROR] " + ioe.getMessage());
+        }
+    }
+}
