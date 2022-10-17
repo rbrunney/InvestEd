0c90e96eacbf03441b1e5c3a27955cbb1f0e0546
rbrunney <rbrunney@student.neumont.edu>
2022-10-17 10:37:57 -0600

    Working on Decoding JWT in Flask
    Trying to decode JWT from Authorization header so we can validate the user for their orders

--- InvestEd/Microservices/Order Service/order_api.py
+++ InvestEd/Microservices/Order Service/order_api.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, request
+import jwt
+import os
 
 order_api = Flask(__name__)
 
 @order_api.route('/invested_order/place_order', methods=['POST'])
 def place_order():
-    pass
+    return decode_json_web_token(request.headers["authorization"].replace('Bearer ', ''))
 
 @order_api.route('/invested_order/get_order_info/<order_id>', methods=['GET'])
 def get_order(order_id: str):
@@ -21,6 +23,16 @@ def cancel_order(order_info: str):
 @order_api.route('/invested_order/cancel_all_orders', methods=['DELETE'])
 def cancel_all_orders():
     pass
+
+def decode_json_web_token(incoming_jwt:str):
+    decoded_jwt = jwt.decode(
+            jwt=incoming_jwt, 
+            key=str(os.getenv('JWT_SECERT')), 
+            algorithms=[str(os.getenv('JWT_ALGORITHM'))], 
+            issuer=str(os.getenv('JWT_ISSUER'))
+    )
+    return decoded_jwt
+    
 
 if __name__ == '__main__':
     order_api.run(host='0.0.0.0', port=105)
