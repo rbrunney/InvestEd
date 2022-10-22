8c1235fc9bfda38d774e091101a833e713eac7e7
rbrunney <rbrunney@student.neumont.edu>
2022-10-22 13:06:06 -0600

    Security Added for Portfolio API
    Added Base Filter and SecurityConfig for Portfolio API

---
+++ InvestEd/Microservices/Portfolio Service/PortfolioService/src/main/java/com/invested/portfolioservice/config/security/SecurityConfig.java
@@ -0,0 +1,63 @@
+package com.invested.portfolioservice.config.security;
+
+import com.invested.portfolioservice.config.filter.CustomAuthorizationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    // To Keep track of current active users
+    private final InMemoryUserDetailsManager memAuth = new InMemoryUserDetailsManager();
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http, UserDetailsService userDetailsService) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailsService) // Pass in the userDetails service we are going to be keeping track off
+                .passwordEncoder(passwordEncoder()) // Defaulting password encryption to BCrypt
+                .and()
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager(); // Getting Authentication manager context
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.csrf().disable()
+                .httpBasic(Customizer.withDefaults())
+                .authorizeRequests()
+                .anyRequest().permitAll()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(); // Using Bcrypt for any user for there passwords
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return memAuth;
+    }
+}
