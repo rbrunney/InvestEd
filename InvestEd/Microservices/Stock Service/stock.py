bbc58f9f014c7948ed2fd37d9e9a4165c53e99b9
rbrunney <rbrunney@student.neumont.edu>
2022-10-29 19:11:47 -0600

    Updated Basic Info
    Updated most of Basic Info just have to see how I can get the rest of the information I want. Moving onto the news

--- InvestEd/Microservices/Stock Service/stock.py
+++ InvestEd/Microservices/Stock Service/stock.py
@@ -12,41 +12,30 @@ class Stock:
 
     def __init__(self, ticker : str):
         self.ticker = ticker
-        self.client = RESTClient(os.getenv("POLYGON_API_KEY"))
+        self.client = RESTClient()
+
+    def check_date(self, current_date=current_dt.today()):
+        saturday = 5
+        sunday = 6
+        date_value = current_date.weekday()
+
+        if date_value == saturday:
+            return current_date - dt.timedelta(days=1)
+        elif date_value == sunday:
+            return current_date - dt.timedelta(days=2)
+
+        return current_date
 
     def get_current_price(self):
 
-        def check_date(current_date=current_dt.today()):
-            saturday = 5
-            sunday = 6
-            date_value = current_date.weekday()
-
-            if date_value == saturday:
-                return current_date - dt.timedelta(days=1)
-            elif date_value == sunday:
-                return current_date - dt.timedelta(days=2)
-
-            return current_date
-        
         # Getting the date and then checking to get the the current price, at least close to it
-        date_to_retrieve = check_date()
+        date_to_retrieve = self.check_date()
         aggs = self.client.get_aggs(self.ticker, 1, "day", date_to_retrieve, date_to_retrieve)
         return aggs[0].vwap
     
     def get_data_points(self, period):
 
-        def check_date(current_date=current_dt.today()):
-            saturday = 5
-            sunday = 6
-            date_value = current_date.weekday()
-
-            if date_value == saturday:
-                return current_date - dt.timedelta(days=1)
-            elif date_value == sunday:
-                return current_date - dt.timedelta(days=2)
-
-            return current_date
-
+        # Getting the aggreagate data so we can return array of datapoints later
         def period_data_points(from_date, end_date, timespan):
             aggregates = self.client.get_aggs(self.ticker, multiplier=1, from_=from_date, to=end_date, timespan=timespan,limit=250)
             data_points = []
@@ -57,17 +46,17 @@ class Stock:
             return data_points
 
         if period == "DAY":
-            return period_data_points(check_date(), check_date(), "minute")
+            return period_data_points(self.check_date(), self.check_date(), "minute")
         elif period == "WEEK":
-            return period_data_points(check_date() - dt.timedelta(days=7), check_date(), "minute")
+            return period_data_points(self.check_date() - dt.timedelta(days=7), self.check_date(), "minute")
         elif period == "MONTH":
-            return period_data_points(check_date() - dt.timedelta(days=31), check_date(), "minute")
+            return period_data_points(self.check_date() - dt.timedelta(days=31), self.check_date(), "minute")
         elif period == "3-MONTH":
-            return period_data_points(check_date() - dt.timedelta(days=93), check_date(), "minute")
+            return period_data_points(self.check_date() - dt.timedelta(days=93), self.check_date(), "minute")
         elif period == "YEAR":
-            return period_data_points(check_date() - dt.timedelta(days=365), check_date(), "day")
+            return period_data_points(self.check_date() - dt.timedelta(days=365), self.check_date(), "day")
         elif period == "5-YEAR":
-            return period_data_points(check_date() - dt.timedelta(days=1825), check_date(), "day")
+            return period_data_points(self.check_date() - dt.timedelta(days=1825), self.check_date(), "day")
         else:
             # Return 400 so we know we have to send a bad request
             return 400
@@ -78,52 +67,34 @@ class Stock:
         basic_info = {}
 
         # Getting first request data for part of basic info
-        first_request = requests.get(f'https://www.alphavantage.co/query?function=OVERVIEW&symbol={self.ticker}&apikey={os.getenv("ALPHA_VANTAGE_API_KEY")}')
-        fetched_data = first_request.json()
+        ticker_details = self.client.get_ticker_details(ticker=self.ticker)
 
         # Adding information into basic_info
-        basic_info['ticker'] = fetched_data['Symbol']
-        basic_info['52_week_high'] = float(fetched_data['52WeekHigh'])
-        basic_info['52_week_low'] = float(fetched_data['52WeekLow'])
-        basic_info['market_cap'] = int(fetched_data['MarketCapitalization'])
-        basic_info['pe_ratio'] = float(fetched_data['PERatio'])
-        basic_info['dividend_yeild'] = float(fetched_data['DividendYield'])
-        basic_info['description'] = fetched_data['Description']
-        basic_info['hq_city'] = fetched_data['Address'].split(',')[1].replace(' ', '')
-        basic_info['hq_state'] = fetched_data['Address'].split(',')[2].replace(' ', '')
-        basic_info['sector'] = fetched_data['Sector']
-        basic_info['industry'] = fetched_data['Industry']
+        basic_info['ticker'] = ticker_details.ticker
+        basic_info['name'] = ticker_details.name
+        # basic_info['52_week_high'] = float(fetched_data['52WeekHigh'])
+        # basic_info['52_week_low'] = float(fetched_data['52WeekLow'])
+        basic_info['market_cap'] = float(ticker_details.market_cap)
+        # basic_info['pe_ratio'] = float(fetched_data['PERatio'])
+        # basic_info['dividend_yeild'] = float(fetched_data['DividendYield'])
+        basic_info['description'] = ticker_details.description
+        basic_info['list_date'] = ticker_details.list_date
+        basic_info['total_employees'] = ticker_details.total_employees
+        basic_info['hq_address'] = {
+            "street" : ticker_details.address.address1,
+            "city" : ticker_details.address.city,
+            "state" : ticker_details.address.state,
+            "zipcode" : ticker_details.address.postal_code
+        }
 
         # Making second request for current days open, high, and low
-        second_request = requests.get(f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={self.ticker}&interval=5min&apikey={os.getenv("ALPHA_VANTAGE_API_KEY")}')
-        fetched_data = second_request.json()
-
-        open = 0
-        high = 0
-        low = sys.maxsize
-        volume = 0
-
-        # Need to loop through all keys
-        for key in fetched_data["Time Series (5min)"].keys():
-            if (open == 0):
-                open = float(fetched_data["Time Series (5min)"][key]["1. open"])
-
-            # Check to see if its hit the highest yet
-            if (high < float(fetched_data["Time Series (5min)"][key]["2. high"])):
-                high = float(fetched_data["Time Series (5min)"][key]["2. high"])
-
-            # Check to see if its hit the lowest yet
-            if (low > float(fetched_data["Time Series (5min)"][key]["3. low"])):
-                low = float(fetched_data["Time Series (5min)"][key]["3. low"])
-
-            # Adding volume traded to the total volue traded
-            volume += float(fetched_data["Time Series (5min)"][key]["5. volume"])
+        daily_info = self.client.get_daily_open_close_agg(ticker=self.ticker, date=self.check_date().date())
         
         # Adding open, high, low, and volume to basic information
-        basic_info['open'] = open
-        basic_info['high'] = high
-        basic_info['low'] = low
-        basic_info['volume'] = volume
+        basic_info['open'] = daily_info.open
+        basic_info['high'] = daily_info.high
+        basic_info['low'] = daily_info.close
+        basic_info['volume'] = daily_info.volume
 
         return basic_info
 
@@ -210,4 +181,4 @@ class Stock:
             'moving_avg_data' : moving_avg_data_points
         }
     
-print(Stock("VOO").get_data_points(""))
+print(Stock("AAPL").get_basic_info())
