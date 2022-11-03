f0a1b8c9d8cec3d82efb4bbdaeb99e16d92fbd23
rbrunney <rbrunney@student.neumont.edu>
2022-11-02 20:56:47 -0600

    Post and Get for Lesson working
    Base lesson Post and Get now working, just have to work on other models to get it fully complete

--- InvestEd/Microservices/Lesson Service/LessonService/src/main/java/org/invested/lessonservice/controllers/LessonController.java
+++ InvestEd/Microservices/Lesson Service/LessonService/src/main/java/org/invested/lessonservice/controllers/LessonController.java
@@ -1,15 +1,37 @@
 package org.invested.lessonservice.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.invested.lessonservice.models.Lesson;
+import org.invested.lessonservice.services.LessonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/invested_lesson")
 public class LessonController {
 
-    @GetMapping("/test")
-    public String test() {
-        return "/test";
+    @Autowired
+    private LessonService lessonService;
+
+    @PostMapping()
+    public ResponseEntity<Map<String, Object>> createLesson(@RequestBody Lesson lesson) {
+        lessonService.saveLesson(lesson);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{lessonName}")
+    public ResponseEntity<Map<String, Object>> getLesson(@PathVariable String lessonName) {
+        Map<String, Object> response = new HashMap<>() {{
+            put("message", lessonName + " has been retrieved successfully");
+            put("results", lessonService.getLesson(lessonName));
+            put("date-time", LocalDateTime.now());
+        }};
+        
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
--- InvestEd/Microservices/Lesson Service/LessonService/src/main/java/org/invested/lessonservice/repositories/LessonJPARepository.java
+++ InvestEd/Microservices/Lesson Service/LessonService/src/main/java/org/invested/lessonservice/repositories/LessonJPARepository.java
@@ -4,4 +4,5 @@ import org.invested.lessonservice.models.Lesson;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LessonJPARepository extends JpaRepository<Lesson, String> {
+    Lesson getLessonByName(String lessonName);
 }
