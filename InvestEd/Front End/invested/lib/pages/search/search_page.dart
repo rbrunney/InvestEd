6bb1dffcca0a626d0bb523039d7c785efe199755
rbrunney <rbrunney@student.neumont.edu>
2022-11-23 00:40:20 -0700

    Working on Search Page
    Working on Search Page, trying to get a news article to show up in web page

--- InvestEd/Front End/invested/lib/pages/search/search_page.dart
+++ InvestEd/Front End/invested/lib/pages/search/search_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:invested/pages/search/news_article_card.dart';
+import 'package:invested/util/custom_text_field.dart';
+import 'package:invested/util/page_title.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({Key? key}) : super(key: key);
@@ -8,8 +11,35 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+
+  TextEditingController searchController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+        child: Scaffold(
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                CustomTextField(
+                  prefixIcon: Icons.search,
+                  hintText: "Find Ticker...",
+                  labelText: "Find Ticker",
+                  textCallBack: (value) {},
+                  textController: searchController
+                ),
+                const PageTitle(
+                  alignment: Alignment.center,
+                    title: "Current News",
+                  fontSize: 30,
+                ),
+                const NewsArticleCard(
+                  thumbnailURL: "https://i-invdn-com.investing.com/redesign/images/seo/investingcom_analysis_og.jpg",
+                )
+              ],
+            ),
+          ),
+        )
+    );
   }
 }
