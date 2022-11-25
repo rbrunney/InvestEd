7a96b5efa91c69989fe75894ea279d20859417b8
rbrunney <rbrunney@student.neumont.edu>
2022-11-25 01:17:45 -0700

    Worked on AWS Hosting
    Worked on AWS hosting

--- InvestEd/Microservices/image_upload_aws.bat
+++ InvestEd/Microservices/image_upload_aws.bat
@@ -1,20 +1,28 @@
 :: Checking to see if connection is valid with login sucess
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 884873584882.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region us-west-1 | docker login --username AWS --password-stdin 884873584882.dkr.ecr.us-west-1.amazonaws.com/invested
 
 :: /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 :: API's
 
 :: Stock API
-docker tag invested-stock-api:1.0 884873584882.dkr.ecr.us-east-1.amazonaws.com/invested:stock-api
-docker push 884873584882.dkr.ecr.us-east-1.amazonaws.com/invested:stock-api
+docker tag invested-stock-api:1.0 884873584882.dkr.ecr.us-west-1.amazonaws.com/invested:stock-api
+docker push 884873584882.dkr.ecr.us-west-1.amazonaws.com/invested:stock-api
 
 :: Account API
-docker tag invested-account-api:1.0 884873584882.dkr.ecr.us-east-1.amazonaws.com/invested:account-api
-docker push 884873584882.dkr.ecr.us-east-1.amazonaws.com/invested:account-api
+docker tag invested-account-api:1.0 884873584882.dkr.ecr.us-west-1.amazonaws.com/invested:account-api
+docker push 884873584882.dkr.ecr.us-west-1.amazonaws.com/invested:account-api
 
 :: Portfolio API
-docker tag invested-portfolio-api:1.0 884873584882.dkr.ecr.us-east-1.amazonaws.com/invested:portfolio-api
-docker push 884873584882.dkr.ecr.us-east-1.amazonaws.com/invested:portfolio-api
+docker tag invested-portfolio-api:1.0 884873584882.dkr.ecr.us-west-1.amazonaws.com/invested:portfolio-api
+docker push 884873584882.dkr.ecr.us-west-1.amazonaws.com/invested:portfolio-api
+
+:: Order API
+docker tag invested-order-api:1.0 884873584882.dkr.ecr.us-west-1.amazonaws.com/invested:order-api
+docker push 884873584882.dkr.ecr.us-west-1.amazonaws.com/invested:order-api
+
+:: Lesson API
+docker tag invested-lesson-api:1.0 884873584882.dkr.ecr.us-west-1.amazonaws.com/invested:lesson-api
+docker push 884873584882.dkr.ecr.us-west-1.amazonaws.com/invested:lesson-api
 
 :: /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 :: Consumers
@@ -23,13 +31,13 @@ docker push 884873584882.dkr.ecr.us-east-1.amazonaws.com/invested:portfolio-api
 :: Third Party Distributed Systems
 
 :: Gateway
-docker tag invested-gateway:1.0 884873584882.dkr.ecr.us-east-1.amazonaws.com/invested:gateway
-docker push 884873584882.dkr.ecr.us-east-1.amazonaws.com/invested:gateway
+docker tag invested-gateway:1.0 884873584882.dkr.ecr.us-west-1.amazonaws.com/invested:gateway
+docker push 884873584882.dkr.ecr.us-west-1.amazonaws.com/invested:gateway
 
-:: Eureka
-docker tag steeltoeoss/eureka-server:latest 884873584882.dkr.ecr.us-east-1.amazonaws.com/invested:eureka
-docker push 884873584882.dkr.ecr.us-east-1.amazonaws.com/invested:eureka
+@REM :: Eureka
+@REM docker tag steeltoeoss/eureka-server:latest 884873584882.dkr.ecr.us-west-1.amazonaws.com/invested:eureka
+@REM docker push 884873584882.dkr.ecr.us-west-1.amazonaws.com/invested:eureka
 
-:: RabbitMQ
-docker tag rabbitmq:3-management 884873584882.dkr.ecr.us-east-1.amazonaws.com/invested:rabbitmq
-docker push 884873584882.dkr.ecr.us-east-1.amazonaws.com/invested:rabbitmq
+@REM :: RabbitMQ
+@REM docker tag rabbitmq:3-management 884873584882.dkr.ecr.us-west-1.amazonaws.com/invested:rabbitmq
+@REM docker push 884873584882.dkr.ecr.us-west-1.amazonaws.com/invested:rabbitmq
