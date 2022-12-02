fe955cc4ad68a5e345e57e74333562f618ae5e40
rbrunney <rbrunney@student.neumont.edu>
2022-12-01 17:28:17 -0700

    Fixed Appearance Error
    Fixed Appearance Error! So know light and dark mode is good!

--- InvestEd/Front End/invested/lib/pages/account/account_page.dart
+++ InvestEd/Front End/invested/lib/pages/account/account_page.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 import 'package:invested/pages/account/account_header.dart';
 import 'package:invested/pages/account/account_settings/account_settings.dart';
 import 'package:invested/pages/account/logout_settings/logout_settings.dart';
+import 'package:invested/pages/account/settings_tab.dart';
+import 'package:invested/pages/account/support_settings/contact_page.dart';
+import 'package:invested/pages/account/support_settings/privacy_policy.dart';
 import 'package:invested/pages/account/support_settings/support_settings.dart';
+import 'package:page_transition/page_transition.dart';
+import 'package:invested/util/global_styling.dart' as global_styling;
+
+import '../../util/custom_text.dart';
+import '../../util/page_title.dart';
+import '../login/forgot_password_page.dart';
+import '../login/login_page.dart';
+import 'account_settings/account_info_page.dart';
+import 'account_settings/order_history_page.dart';
 
 class AccountPage extends StatefulWidget {
   const AccountPage({Key? key}) : super(key: key);
@@ -18,11 +31,164 @@ class _AccountPageState extends State<AccountPage> {
         child: Scaffold(
             body: SingleChildScrollView(
               child: Column(
-                children: const [
-                  AccountHeader(),
-                  AccountSettings(),
-                  SupportSettings(),
-                  LogoutSettings()
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(top: 65, left: 20, right: 20, bottom: 65),
+                    child: Row(
+                        children: [
+                          const Icon(
+                            Icons.account_circle_outlined,
+                            size: 60,
+                          ),
+                          Container(
+                              margin: const EdgeInsets.only(left: 10),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  CustomText(
+                                    text: "Robert Brunney",
+                                    fontSize: 25,
+                                  ),
+                                  CustomText(
+                                      text: "Member Since: 11-15-2022",
+                                      fontSize: 14,
+                                      color: Colors.grey
+                                  )
+                                ],
+                              )
+                          )
+                        ]
+                    ),
+                  ),
+                  Container(
+                      margin: const EdgeInsets.only(left: 15, right: 15, bottom: 20),
+                      child: Column(
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.only(left: 15),
+                            child: const PageTitle(
+                              title: "Account",
+                              fontSize: 20,
+                            ),
+                          ),
+                          SettingsTab(
+                            iconData: Icons.account_circle_outlined,
+                            name: "Account Info",
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  PageTransition(
+                                      child: const AccountInfoPage(),
+                                      type: PageTransitionType.rightToLeftWithFade));
+                            },
+                          ),
+                          SettingsTab(
+                            iconData: Icons.history,
+                            name: "Order History",
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  PageTransition(
+                                      child: const OrderHistoryPage(),
+                                      type: PageTransitionType.rightToLeftWithFade));
+                            },
+                          ),
+                          Container(
+                              margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                              child: Row(
+                                children: [
+                                  Icon(global_styling.ThemeChanger.isDark ? Ionicons.md_moon_outline : Ionicons.md_sunny_outline),
+                                  CustomText(
+                                    leftMargin: 20,
+                                    text: "Appearance",
+                                    fontSize: 18,
+                                  ),
+                                  const Spacer(),
+                                  Switch(
+                                      activeColor: Color(global_styling.LOGO_COLOR),
+                                      value: global_styling.ThemeChanger.isDark,
+                                      onChanged: (value) => setState(() {
+                                        global_styling.currentTheme.switchTheme();
+                                      }
+                                      )
+                                  )
+                                ],
+                              )
+                          ),
+                          SettingsTab(
+                            iconData: Icons.lock_outline,
+                            name: "Password",
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  PageTransition(
+                                      child: const ForgotPasswordPage(title: "Update",),
+                                      type: PageTransitionType.rightToLeftWithFade));
+                            },
+                          ),
+                        ],
+                      )
+                  ),
+                  Container(
+                      margin:  const EdgeInsets.only(left: 15, right: 15, bottom: 20),
+                      child: Column(
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.only(left: 15),
+                            child: const PageTitle(
+                              title: "Help & Support",
+                              fontSize: 20,
+                            ),
+                          ),
+                          SettingsTab(
+                            iconData: Icons.email_outlined,
+                            name: "Contact",
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  PageTransition(
+                                      child: const ContactPage(),
+                                      type: PageTransitionType.rightToLeftWithFade));
+                            },
+                          ),
+                          SettingsTab(
+                            iconData: Icons.notes_outlined,
+                            name: "Privacy Policy",
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  PageTransition(
+                                      child: const PrivacyPolicy(),
+                                      type: PageTransitionType.rightToLeftWithFade));
+                            },
+                          )
+                        ],
+                      )
+                  ),
+                  Container(
+                      margin:  const EdgeInsets.only(left: 15, right: 15, bottom: 20),
+                      child: Column(
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.only(left: 15),
+                              child: const PageTitle(
+                                title: "Logout",
+                                fontSize: 20,
+                              ),
+                            ),
+                            SettingsTab(
+                              iconData: MaterialCommunityIcons.exit_run,
+                              name: "Log Out",
+                              onTap: () {
+                                Navigator.of(context).pushAndRemoveUntil(
+                                    MaterialPageRoute(
+                                        builder: (context) => const LoginPage()),
+                                        (Route<dynamic> route) => false);
+                              },
+                            )
+                          ]
+                      )
+                  )
                 ],
               )
             )
--- InvestEd/Front End/invested/lib/pages/lesson/lesson_page.dart
+++ InvestEd/Front End/invested/lib/pages/lesson/lesson_page.dart
@@ -1,7 +1,14 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 import 'package:invested/pages/lesson/lesson_marker.dart';
 import 'package:invested/util/page_title.dart';
+import 'package:invested/util/requests.dart';
+import 'package:invested/util/global_styling.dart' as global_styling;
+import 'package:invested/util/global_info.dart' as global_info;
+import 'package:path_provider/path_provider.dart';
 
 class LessonPage extends StatefulWidget {
   const LessonPage({Key? key}) : super(key: key);
@@ -11,51 +18,102 @@ class LessonPage extends StatefulWidget {
 }
 
 class _LessonPageState extends State<LessonPage> {
+  late List<double> percentagesCompleted = [];
+  int totalSubCategoryCount = 0;
 
   @override
   void initState() {
     super.initState();
+    getLessonInformation();
+  }
 
+  Future<void> getLessonInformation() async {
+    List<String> lessonNames = [
+      "Evaluating Goals",
+      "Risk Management",
+      "Stock 101",
+      "Order Types"
+    ];
+
+    for (String lessonName in lessonNames) {
+      Requests.makeGetRequest(
+          "${global_info.localhost_url}/invested_lesson/$lessonName")
+          .then((value) async {
+        var response = json.decode(value);
+
+        String completedLessons = '';
+        int totalCompleted = 0;
+
+        final Directory directory = await getApplicationDocumentsDirectory();
+        final File file = File('${directory.path}/${global_info.username}.txt');
+
+        try {
+          completedLessons = await file.readAsString();
+        } catch (e) {
+          completedLessons = '';
+        }
+
+        for (int i = 0; i < response['lessonInfo'].length; i++) {
+          setState(() {
+            bool completedLesson = completedLessons.contains(
+                response['lessonInfo'][i]['header']);
+            if (completedLesson) {
+              totalCompleted += 1;
+            }
+          });
+        }
+
+        setState(() {
+          totalSubCategoryCount = response['lessonInfo'].length + 1;
+          percentagesCompleted.add((totalCompleted / totalSubCategoryCount) * 100);
+        });
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-          body: SingleChildScrollView(
-            child: Column(
-              children: [
-                Container(
-                  margin: const EdgeInsets.only(top: 25, left: 15),
-                  child: const PageTitle(title: "All Lessons"),
-                ),
-                const LessonMarker(
-                  iconData: MaterialCommunityIcons.checkbox_marked_outline,
-                  name: "Evaluating Goals",
-                  reward: 500,
-                  percentageComplete: 0,
-                ),
-                const LessonMarker(
-                  iconData: MaterialCommunityIcons.alert_octagram_outline,
-                  name: "Risk Management",
-                  reward: 750,
-                  percentageComplete: 0,
-                ),
-                const LessonMarker(
-                  iconData: Ionicons.md_cash_outline,
-                  name: "Stock 101",
-                  reward: 1000,
-                  percentageComplete: 0,
-                ),
-                const LessonMarker(
-                  iconData: MaterialCommunityIcons.script_text_outline,
-                  name: "Order Types",
-                  reward: 350,
-                  percentageComplete: 0,
-                )
-              ],
+            body: RefreshIndicator(
+              color: Color(global_styling.LOGO_COLOR),
+              onRefresh: getLessonInformation,
+              child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(top: 25, left: 15),
+                        child: const PageTitle(title: "All Lessons"),
+                      ),
+                      LessonMarker(
+                        iconData: MaterialCommunityIcons
+                            .checkbox_marked_outline,
+                        name: "Evaluating Goals",
+                        reward: 500,
+                        percentageComplete: (percentagesCompleted.isNotEmpty) ? percentagesCompleted[0] : 0,
+                      ),
+                      LessonMarker(
+                        iconData: MaterialCommunityIcons.alert_octagram_outline,
+                        name: "Risk Management",
+                        reward: 750,
+                        percentageComplete: (percentagesCompleted.isNotEmpty && percentagesCompleted.length > 1) ? percentagesCompleted[1] : 0,
+                      ),
+                      LessonMarker(
+                        iconData: Ionicons.md_cash_outline,
+                        name: "Stock 101",
+                        reward: 1000,
+                        percentageComplete: (percentagesCompleted.isNotEmpty && percentagesCompleted.length > 2) ? percentagesCompleted[2] : 0,
+                      ),
+                      LessonMarker(
+                        iconData: MaterialCommunityIcons.script_text_outline,
+                        name: "Order Types",
+                        reward: 350,
+                        percentageComplete: (percentagesCompleted.isNotEmpty && percentagesCompleted.length > 3) ? percentagesCompleted[3] : 0,
+                      )
+                    ],
+                  )
+              ),
             )
-          ),
         )
     );
   }
